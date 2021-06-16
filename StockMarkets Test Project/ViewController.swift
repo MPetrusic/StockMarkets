78bcc0cea9e3cd4cd19ff45f2008e413e0eb5c33
Milos Petrusic <petrusicmilos94@gmail.com>
2021-06-16 14:21:26 +0200

    Initial Commit

---
+++ StockMarkets Test Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockMarkets Test Project
+//
+//  Created by Milos Petrusic on 16.6.21..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
